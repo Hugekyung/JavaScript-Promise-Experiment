b359d1d174f227b1440450a0755c79b0d775eeb1
haechan <kiki9510@gmail.com>
2022-06-02 00:21:56 +0900

    Promise를 활용한 비동기  병렬처리

--- promise.all-2.js
+++ promise.all-2.js
@@ -1,3 +1,61 @@
 // * Promise.all()은 모두 성공하거나, 하나라도 실패하는 경우 실패한 프로미스를 반환하게 되는데
 // ? 실패한 이후 나머지 프로미스 작업들은 그 순간 멈출까, 아니면 끝까지 수행될까?
 // ? 비동기처리를 통해 DB에 접근하게 될 경우, 어느 하나가 실패했는데 나머지들이 계속 수행되어 커밋된다면 문제가 될 수 있다..
+
+// * 2초가 되면서 에러가 발생하지만, Promise.all()은 에러와 관계없이 모든 프로미스를 이행한 뒤 순서에 맞춰 값을 반환
+const testArr = [
+    new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)), // 1
+    new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)), // 2
+    new Promise(
+        (resolve, reject) =>
+            setTimeout(() => reject(new Error("Error !")), 2000) // 2초에서 에러가 발생했지만, Promise.all()은 전체를 모두 수행하는듯?
+    ), // 2
+    new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000)), // 3
+];
+
+async function PromiseTest1() {
+    const newTestArr = await Promise.all(
+        testArr.map(async (promise) => {
+            return await promise.catch((error) => error.message);
+        })
+    );
+
+    console.log(newTestArr);
+}
+
+// * 비동기처리 에제 - for loop
+// for loop를 통한 프로미스 처리
+async function PromiseTest2() {
+    const newArr = [];
+    for (promise of testArr) {
+        await promise.then((result) => newArr.push(result)).catch(console.log);
+    }
+
+    console.log(newArr);
+}
+
+PromiseTest1(); // [ 101, 102, 103 ]
+PromiseTest2(); // [ 1, 2, 3 ]
+
+// * Promise.allSettled()
+async function PromiseTest3() {
+    const newTestArr = await Promise.allSettled(
+        testArr.map(async (promise) => {
+            return await promise;
+        })
+    );
+
+    console.log("Promise.allSettled() >>", newTestArr);
+}
+
+async function PromiseTest4() {
+    const newArr = [];
+    for (promise of testArr) {
+        await promise.then((result) => newArr.push(result)).catch(console.log);
+    }
+
+    console.log("for loop >>", newArr);
+}
+
+PromiseTest3();
+PromiseTest4();
--- promise.for-loop.js
+++ promise.for-loop.js
@@ -4,6 +4,7 @@ const testArr = [
     new Promise((resolve) => setTimeout(() => resolve(3), 1000)), // 3
 ];
 
+// * 비동기처리 실패 예제
 // map - map 함수 자체는 배열을 돌면서 순서대로 값을 처리하고 있고, map 내부에서 await을 통해 비동기처리를 해줄 뿐이다.
 // map 자체를 비동기처리해주지 않으면 비동기처리가 되기도 전에 map 함수는 다음 값을 실행해버린다.
 // 결론적으로 pending상태인 배열을 리턴하게 된다.
@@ -16,29 +17,31 @@ async function PromiseTest() {
     console.log(newTestArr);
 }
 
+// * 비동기처리 에제 - map
 // map 함수 전체를 Promise.all()로 감싸 비동기처리를 하고 있고, map 함수 내부에서 각각의 루프마다 비동기처리를 해주고 있다.
 // 따라서 모든 배열을 돌며 모두 성공하거나 하나라도 실패할 경우 프로미스 결과를 반환하게 한다.
+// 시간상 3번째 프로미스가 가장 빨리 종료하지만, Promise.all()은 순서를 보장하기 때문에 순서에 맞춰 리턴한다.
 async function PromiseTest2() {
     const newTestArr = await Promise.all(
         testArr.map(async (promise) => {
-            const newPromise = (await promise) + 100;
-            return newPromise;
+            return await promise;
         })
     );
 
-    console.log(newTestArr);
+    console.log("Promise.all() - map >>", newTestArr);
 }
 
-// 단순 for loop를 통한 프로미스 처리
+// * 비동기처리 에제 - for loop
+// for loop를 통한 프로미스 처리
 async function PromiseTest3() {
     const newArr = [];
     for (promise of testArr) {
-        await promise.then((result) => newArr.push(result));
+        await promise.then((result) => newArr.push(result)).catch(console.log);
     }
 
-    console.log(newArr);
+    console.log("for loop >>", newArr);
 }
 
 PromiseTest(); // [ Promise { <pending> }, Promise { <pending> }, Promise { <pending> } ]
 PromiseTest2(); // [ 101, 102, 103 ]
-PromiseTest3(); // [ 1, 2, 3 ] => 시간상 3번째 프로미스가 가장 빨리 종료하지만, Promise.all()은 순서를 보장하기 때문에 순서에 맞춰 리턴한다.
+PromiseTest3(); // [ 1, 2, 3 ]
